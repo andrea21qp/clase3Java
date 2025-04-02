e896b34bbf9953745a5f3cb14a22508253e7013f
Karen-404 <tat.3012.mc@gmail.com>
2025-04-02 11:01:54 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.miprimerprograma;
+
+/**
+ *
+ * @author SO-LAB1-PC19
+ */
+public class MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        
+        int num=5;
+        int opercacion =num%2;
+        if(operacion==0)
+        System.out.println("el numero es par");
+    }
+    else {
+            System.out.println("el numero es impar");
+            
+         
+    }
+}
+
+
